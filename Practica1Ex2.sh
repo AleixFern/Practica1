b4f21b5f52ff654268675a540a75a7fe389006e7
AleixFern <afernandezm@ies-sabadell.cat>
2025-04-25 19:19:27 +0200

    Add files via upload

---
+++ Practica1Ex2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Enter the name of the variable: " var_name
+
+read -p "Enter the value for $var_name: " var_value
+
+echo "The variable '$var_name' has the value: '$var_value'"
